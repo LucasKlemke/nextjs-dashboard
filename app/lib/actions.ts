817565a33ce2d6b4ccb1ab6cde40cdba5b2471c0
Lucas Klemke <lucas.klemke@Outlook.com.br>
2024-08-21 14:04:19 -0300

    Add edit invoice function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+
+
  
 const FormSchema = z.object({
   id: z.string(),
@@ -14,6 +16,8 @@ const FormSchema = z.object({
 });
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
     // const rawFormData = {
@@ -38,4 +42,23 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/invoices');
       console.log(amount);
       console.log(typeof amount);
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
